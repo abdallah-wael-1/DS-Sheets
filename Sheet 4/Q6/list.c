4f26ddc0458f4efc11d1b0d259bc8a35669d4997
Abdallah Wael El Saadany <abdallahelsadany18@gmail.com>
2025-11-25 18:31:22 +0200

    Update list.c
    Added functions to create, check if empty, get size, insert, and delete elements in a list.

--- Sheet 4/Q6/list.c
+++ Sheet 4/Q6/list.c
@@ -1,6 +1,66 @@
 #include "list.h"
 #include <stdlib.h>
 
+void createList(List *pl) {
+    pl->head = NULL;
+    pl->size = 0;
+}
+
+int isEmptyList(List *pl) {
+    return pl->size == 0;
+}
+
+int listSize(List *pl) {
+    return pl->size;
+}
+
+
+void insertList(List *pl, int pos, ListEntry e) {
+    if (pos < 0 || pos > pl->size) return; 
+
+    Node *newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = e;
+    newNode->next = NULL;
+
+    if (pos == 0) { 
+        newNode->next = pl->head;
+        pl->head = newNode;
+    } else {
+        Node *cur = pl->head;
+        for (int i = 0; i < pos - 1; i++) {
+            cur = cur->next;
+        }
+        newNode->next = cur->next;
+        cur->next = newNode;
+    }
+
+    pl->size++;
+}
+
+
+void deleteList(List *pl, int pos, ListEntry *e) {
+    if (pos < 0 || pos >= pl->size || pl->head == NULL) 
+        listSizereturn; 
+
+    Node *toDelete;
+    if (pos == 0) { 
+        toDelete = pl->head;
+        pl->head = pl->head->next;
+    } else {
+        Node *cur = pl->head;
+        for (int i = 0; i < pos - 1; i++) {
+            cur = cur->next;
+        }
+        toDelete = cur->next;
+        cur->next = toDelete->next;
+    }
+
+    *e = toDelete->data; 
+    free(toDelete);
+    pl->size--;
+}
+
+
 void push_back_impl(List *pl, ListEntry e) {
     Node *newNode = (Node*)malloc(sizeof(Node));
     newNode->data = e;
@@ -40,3 +100,4 @@ void traverseList(List *pl, void (*visit)(ListEntry)) {
         current = current->next;
     }
 }
+
